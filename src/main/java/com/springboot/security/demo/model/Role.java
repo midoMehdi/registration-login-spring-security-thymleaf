3dabf0537ec17d607d2ea04062b9e0e9ea3493f2
midoMehdi <elb.mehdi07@gmail.com>
2022-05-12 13:30:18 +0200

    configure entities

--- src/main/java/com/springboot/security/demo/model/Role.java
+++ src/main/java/com/springboot/security/demo/model/Role.java
@@ -1,4 +1,41 @@
 package com.springboot.security.demo.model;
 
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "role")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_role;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
+
+    public Role() {
+    }
+
+    public Role(String name, Collection<User> users) {
+        this.name = name;
+        this.users = users;
+    }
+
+    public int getId_role() {
+        return id_role;
+    }
+
+    public void setId_role(int id_role) {
+        this.id_role = id_role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ src/main/java/com/springboot/security/demo/model/User.java
@@ -0,0 +1,78 @@
+package com.springboot.security.demo.model;
+
+import org.hibernate.procedure.spi.ParameterRegistrationImplementor;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_user;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_role",
+            joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id_user"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id_role")
+    )
+    private Collection<Role> roles;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastname, String email, String password, Collection<Role> roles) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+}
