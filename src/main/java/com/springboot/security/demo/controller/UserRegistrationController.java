3ae034b148fc2cfc90af39dd6a40b57599781734
midoMehdi <elb.mehdi07@gmail.com>
2022-05-12 14:24:24 +0200

    configure service, controller repository for user

---
+++ src/main/java/com/springboot/security/demo/controller/UserRegistrationController.java
@@ -0,0 +1,7 @@
+package com.springboot.security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("/registration")
+public class UserRegistrationController {
+}
---
+++ src/main/java/com/springboot/security/demo/service/UserService.java
@@ -0,0 +1,4 @@
+package com.springboot.security.demo.service;
+
+public interface UserService {
+}
