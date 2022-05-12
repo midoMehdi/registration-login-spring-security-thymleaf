aafcff92d040019c1f65feead9d3732d1e45caac
midoMehdi <elb.mehdi07@gmail.com>
2022-05-12 12:27:52 +0200

    Initial commit

---
+++ src/main/java/com/springboot/security/demo/RegistrationLoginSpringBootThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginSpringBootThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSpringBootThymleafApplication.class, args);
+	}
+
+}
