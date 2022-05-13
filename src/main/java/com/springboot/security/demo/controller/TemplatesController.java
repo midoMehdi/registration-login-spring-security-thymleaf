04679cbbd83cb0dade0bc2aa1d8193370b22c166
midoMehdi <elb.mehdi07@gmail.com>
2022-05-13 15:37:14 +0200

    login logout registration with spring security and spring boot and hibernate and MySQL bench and Thymeleaf done

--- src/main/java/com/springboot/security/demo/controller/TemplatesController.java
+++ src/main/java/com/springboot/security/demo/controller/TemplatesController.java
@@ -6,13 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TemplatesController {
 
+    @GetMapping("/")
+    public String registrationPage(){
+        return "/login";
+    }
     @GetMapping("/login")
     public String loginPage(){
         return "/login";
     }
 
-    @GetMapping("/logout")
-    public String logoutPage(){
-        return "/logout";
+    @GetMapping("/home")
+    public String homePage(){
+        return "/home";
     }
+
+
 }
--- src/main/java/com/springboot/security/demo/security/SecurityConfiguration.java
+++ src/main/java/com/springboot/security/demo/security/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,12 +29,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","registration","/css/*","/js/*").permitAll()
+                .antMatchers("/","/registration","/css/*","/js/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                     .formLogin()
                     .loginPage("/login").permitAll()
-                    .defaultSuccessUrl("/test")
+                    .defaultSuccessUrl("/home")
                 .and()
                     .logout()
                     .invalidateHttpSession(true)
